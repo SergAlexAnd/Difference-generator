0ce9d1ef7aa779f8670523e44cf059fe39aa739f
SergAlexAnd <asergienko@bk.ru>
2019-04-21 13:56:54 +0500

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -5,31 +5,26 @@ import render from './renders';
 const generateKeys = (before, after) => {
   const beforeKeys = Object.keys(before);
   const afterKeys = Object.keys(after);
-  const addedKeys = _.difference(afterKeys, beforeKeys)
-    .map(key => ({ type: 'plus', key, value: after[key] }));
-  const removedKeys = _.difference(beforeKeys, afterKeys)
-    .map(key => ({ type: 'minus', key, value: before[key] }));
-  const commonKeys = _.intersection(beforeKeys, afterKeys)
-    .map((key) => {
-      if (before[key] === after[key]) {
-        return { key, type: 'notChanged', value: after[key] };
-      }
-      const isBeforeHasChildren = typeof before[key] === 'object';
-      const isAfterHasChildren = typeof after[key] === 'object';
-      if (isAfterHasChildren && isBeforeHasChildren) {
-        return {
-          key,
-          type: 'hasChildren',
-          children: generateKeys(before[key], after[key]),
-        };
-      }
+  const addedKeys = _.difference(afterKeys, beforeKeys).map(key => ({ type: 'plus', key, value: after[key] }));
+  const removedKeys = _.difference(beforeKeys, afterKeys).map(key => ({ type: 'minus', key, value: before[key] }));
+  const commonKeys = _.intersection(beforeKeys, afterKeys).map((key) => {
+    if (before[key] === after[key]) return { key, type: 'notChanged', value: after[key] };
+    const isBeforeHasChildren = typeof before[key] === 'object';
+    const isAfterHasChildren = typeof after[key] === 'object';
+    if (isAfterHasChildren && isBeforeHasChildren) {
       return {
-        type: 'changed',
         key,
-        afterValue: after[key],
-        value: before[key],
+        type: 'hasChildren',
+        children: generateKeys(before[key], after[key]),
       };
-    });
+    }
+    return {
+      type: 'changed',
+      key,
+      afterValue: after[key],
+      value: before[key],
+    };
+  });
   return [...commonKeys, ...removedKeys, ...addedKeys];
 };
 
